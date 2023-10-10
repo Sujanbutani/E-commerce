f23f71ba2c3af5995ee2fcf058b76f6d7aae4caa
Sujanbutani <128913845+Sujanbutani@users.noreply.github.com>
2023-10-10 15:14:03 +0530

    development

---
+++ Exam-Practical/src/models/index.js
@@ -0,0 +1,7 @@
+module.exports.User=require("./user.model");
+module.exports.Product=require("./product.model");
+module.exports.Order=require("./order.model");
+module.exports.Cart=require("./cart.model");
+
+
+
---
+++ Exam-Practical/src/services/index.js
@@ -0,0 +1,6 @@
+module.exports.userService=require("./user.service");
+module.exports.productService=require("./product.service");
+module.exports.orderService=require("./order.service");
+module.exports.cartService=require("./cart.service");
+module.exports.emailService=require("./email.services");
+
